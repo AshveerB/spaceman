95dcb0e1c196a0bc1b597bb461f314a14277d0f6
AshveerB <ashveerbhayroo@gmail.com>
2020-10-17 19:17:27 -0700

    add js functionality

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('JS file connected')
+console.log('JS file connected');
+let secretWord = 'test';
+let capitalizeSecretWord = secretWord.toUpperCase();
+let secretWordArray = capitalizeSecretWord.split('');
+console.log(secretWordArray);
+let playerGuess = 'E';
+if (secretWordArray.includes(playerGuess)) {
+	console.log(playerGuess);
+}
+playerGuess = 'A';
+console.log('got here');
